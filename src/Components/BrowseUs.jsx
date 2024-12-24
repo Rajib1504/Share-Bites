272ecee50ade807746ab80d71de9773e0496a5cc
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-12-24 07:52:13 +0530

    one section explore us is done

---
+++ src/Components/BrowseUs.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { IoArrowForwardSharp } from "react-icons/io5";
+
+const BrowseUs = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center mb-8">
+        Browse Our Menu
+      </h1>
+      <div className="border-2 border-red-500 w-full mx-auto md:w-full lg:w-5/6 flex flex-wrap justify-around p-4 items-center gap-6">
+        {/* Breakfast */}
+        <div className="w-64 rounded-lg flex flex-col items-center justify-center border-2 p-6">
+          <div className="rounded-full w-12 mb-4">
+            {/* img */}
+            <div className="rounded-full bg-gray-200 p-2 flex items-center justify-center">
+              <img
+                src="https://i.ibb.co/TLxdfLr/tea-1.png"
+                className="opacity-70 w-8"
+                alt="Breakfast"
+              />
+            </div>
+          </div>
+          {/* text */}
+          <div>
+            <h4 className="font-semibold text-xl mb-2 text-center">
+              Breakfast
+            </h4>
+            <p className="text-xs text-center">
+              In the new era of technology we look in the future with certainty
+              and pride for our life.
+            </p>
+            <div className="text-[#FF5200] hover:text-[#e94a00] flex gap-1 justify-center items-center font-semibold mt-4">
+              <p>Explore menu</p>
+              <IoArrowForwardSharp className="text-xl mt-1" />
+            </div>
+          </div>
+        </div>
+
+        {/* Main Dishes */}
+        <div className="w-64 rounded-lg flex flex-col items-center justify-center border-2 p-6">
+          <div className="rounded-full w-12 mb-4">
+            {/* img */}
+            <div className="rounded-full bg-gray-200 p-2 flex items-center justify-center">
+              <img
+                src="https://i.ibb.co/T0C0dxL/Vector-1.png"
+                className="opacity-70 w-8"
+                alt="Main Dishes"
+              />
+            </div>
+          </div>
+          {/* text */}
+          <div>
+            <h4 className="font-semibold text-xl mb-2 text-center">
+              Main Dishes
+            </h4>
+            <p className="text-xs text-center">
+              In the new era of technology we look in the future with certainty
+              and pride for our life.
+            </p>
+            <div className="text-[#FF5200] hover:text-[#e94a00] flex gap-1 justify-center items-center font-semibold mt-4">
+              <p>Explore menu</p>
+              <IoArrowForwardSharp className="text-xl mt-1" />
+            </div>
+          </div>
+        </div>
+
+        {/* Drinks */}
+        <div className="w-64 rounded-lg flex flex-col items-center justify-center border-2 p-6">
+          <div className="rounded-full w-12 mb-4">
+            {/* img */}
+            <div className="rounded-full bg-gray-200 p-2 flex items-center justify-center">
+              <img
+                src="https://i.ibb.co/2gGW4LT/Vector-2.png"
+                className="opacity-70 w-8"
+                alt="Drinks"
+              />
+            </div>
+          </div>
+          {/* text */}
+          <div>
+            <h4 className="font-semibold text-xl mb-2 text-center">Drinks</h4>
+            <p className="text-xs text-center">
+              In the new era of technology we look in the future with certainty
+              and pride for our life.
+            </p>
+            <div className="text-[#FF5200] hover:text-[#e94a00] flex gap-1 justify-center items-center font-semibold mt-4">
+              <p>Explore menu</p>
+              <IoArrowForwardSharp className="text-xl mt-1" />
+            </div>
+          </div>
+        </div>
+
+        {/* Desserts */}
+        <div className="w-64 rounded-lg flex flex-col items-center justify-center border-2 p-6">
+          <div className="rounded-full w-12 mb-4">
+            {/* img */}
+            <div className="rounded-full bg-gray-200 p-2 flex items-center justify-center">
+              <img
+                src="https://i.ibb.co/DKZSLRL/Group-1.png"
+                className="opacity-70 w-8"
+                alt="Desserts"
+              />
+            </div>
+          </div>
+          {/* text */}
+          <div>
+            <h4 className="font-semibold text-xl mb-2 text-center">Desserts</h4>
+            <p className="text-xs text-center">
+              In the new era of technology we look in the future with certainty
+              and pride for our life.
+            </p>
+            <div className="text-[#FF5200] hover:text-[#e94a00] flex gap-1 justify-center items-center font-semibold mt-4">
+              <p>Explore menu</p>
+              <IoArrowForwardSharp className="text-xl mt-1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrowseUs;
--- src/assets/Pages/Home/Home.jsx
+++ src/assets/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Banner from "../../../Components/Banner";
 import Features from "../../../Components/Features";
+import BrowseUs from "../../../Components/BrowseUs";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      {/* Browse our menu  */}
+      <BrowseUs></BrowseUs>
       {/* Feature section  */}
       <Features></Features>
     </div>
